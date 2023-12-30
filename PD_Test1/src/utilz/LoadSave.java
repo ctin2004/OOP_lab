28eacb05dd2ff648c59c1d1a0ffbad688d497e6d
ctin2004 <ngthanhnam990@gmail.com>
2023-12-30 11:01:09 +0700

    add MenuButton Class

--- PD_Test1/src/utilz/LoadSave.java
+++ PD_Test1/src/utilz/LoadSave.java
@@ -14,6 +14,8 @@ public class LoadSave {
 	public static final String PLAYER_ATLAS = "player_sprites1.png";
 	public static final String LEVEL_ATLAS = "outside_sprites.png";
 	public static final String LEVEL_ONE_DATA = "level_one_data.png";
+	public static final String MENU_BUTTON = "button_atlas.png";
+
 
 	public static BufferedImage GetSpriteAtlas(String fileName) {
 		BufferedImage img = null;
