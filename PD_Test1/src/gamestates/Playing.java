0bfea437db8b2523bdbba1abb9c76b21c95dfa1d
ctin2004 <ngthanhnam990@gmail.com>
2023-12-31 11:37:06 +0700

    Add pause buttons (music,sound,urm,volume)

--- PD_Test1/src/gamestates/Playing.java
+++ PD_Test1/src/gamestates/Playing.java
@@ -7,10 +7,13 @@ import java.awt.event.MouseEvent;
 import entities.Player;
 import levels.LevelManager;
 import main.Game;
+import ui.PauseOverlay;
 
 public class Playing extends State implements Statemethods {
 	private Player player;
 	private LevelManager levelManager;
+	private PauseOverlay pauseOverlay;
+	private boolean paused =false;
 
 	public Playing(Game game) {
 		super(game);
@@ -21,13 +24,18 @@ public class Playing extends State implements Statemethods {
 		levelManager = new LevelManager(game);
 		player = new Player(200, 200, (int) (64 * Game.SCALE), (int) (40 * Game.SCALE));
 		player.loadLvlData(levelManager.getCurrentLevel().getLevelData());
+		pauseOverlay = new PauseOverlay(this);
 
 	}
 
 	@Override
 	public void update() {
-		levelManager.update();
-		player.update();
+		if (!paused) {
+			levelManager.update();
+			player.update();
+		} else {
+			pauseOverlay.update();
+		}
 
 	}
 
@@ -35,6 +43,8 @@ public class Playing extends State implements Statemethods {
 	public void draw(Graphics g) {
 		levelManager.draw(g);
 		player.render(g);
+		if (paused)
+			pauseOverlay.draw(g);
 
 	}
 
@@ -58,7 +68,7 @@ public class Playing extends State implements Statemethods {
 			break;
 			//move back to menu
 		case KeyEvent.VK_BACK_SPACE:
-			Gamestate.state = Gamestate.MENU;
+			paused = !paused;
 			break;
 		}
 	}
@@ -78,25 +88,30 @@ public class Playing extends State implements Statemethods {
 		}
 
 	}
-
+	public void mouseDragged(MouseEvent e) {
+		if (paused)
+			pauseOverlay.mouseDragged(e);
+	}
 	@Override
 	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-
+		if (paused)
+			pauseOverlay.mousePressed(e);
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		// TODO Auto-generated method stub
-
+		if (paused)
+			pauseOverlay.mouseReleased(e);
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		// TODO Auto-generated method stub
-
+		if (paused)
+			pauseOverlay.mouseMoved(e);
 	}
-
+	public void unpauseGame() {
+		paused = false;
+	}
 	public void windowFocusLost() {
 		player.resetDirBooleans();
 	}
